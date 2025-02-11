4ca89f9d5b39eaa870e1a71d4c2d554ef2a92579
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2025-02-10 22:28:18 -0500

    added navigation stack

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from '../screens/home/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
