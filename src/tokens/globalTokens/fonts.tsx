91bd4ff587f2b0018736c061df9feb9359365ddf
Jhohells Erick Dianderas Lopez <jhohellserick@gmail.com>
2025-02-13 13:34:55 -0500

    Refactorizar estilos de RideButton y rideButtonHelper

--- src/tokens/globalTokens/fonts.tsx
+++ src/tokens/globalTokens/fonts.tsx
@@ -2,4 +2,4 @@ export const fonts = {
   br_sonoma_semi_bold: 'BRSonoma-SemiBold',
   br_sonoma_regular: 'BRSonoma-Regular',
   br_sonoma_bold: 'BRSonoma-Bold',
-};
+} as const;
--- storybook/home/HomeScreen.tsx
+++ storybook/home/HomeScreen.tsx
@@ -1,17 +1,58 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import CustomView from '../../src/components/CustomView/CustomView';
 import RideButton from '../../src/components/RideButton/RideButton';
+import { StyleSheet, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <CustomView>
+    <View style={styles.container}>
       <RideButton
-        text="Ride button"
-        size="medium"
+        text="button"
+        size="xlarge"
         onPress={() => console.log('Pequeño presionado')}
       />
-    </CustomView>
+      <View style={{padding: 8}} />
+      <RideButton
+        text="button"
+        size="large"
+        onPress={() => console.log('Pequeño presionado')}
+      />
+      <View style={{padding: 8}} />
+      <RideButton
+        text="button"
+        size="medium"
+        onPress={() => console.log('Grande presionado')}
+      />
+      <View style={{padding: 8}} />
+      <RideButton
+        text="button"
+        size="small"
+        onPress={() => console.log('Mediano presionado')}
+      />
+      <View style={{padding: 8}} />
+      <RideButton
+        text="Deshabilitado"
+        size="medium"
+        state="disabled"
+        onPress={() => console.log('No debería pasar')}
+      />
+      <View style={{padding: 8}} />
+      <RideButton
+        text="Cargando"
+        size="medium"
+        state="loading"
+        onPress={() => console.log('No debería pasar')}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+  },
+});
 
 export default HomeScreen;
